e3ee4f47d5670d724efe83c1abbb6344bb02f528
rahulsharmaaa <rahul__sharma@outlook.com>
2018-10-20 20:01:48 -0400

    Updated IMG urls

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 let scores, roundScore, activePlayer, gamePlaying;
 
 function init() {
@@ -55,7 +44,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // 2. display no.
     let diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block' ;
-    diceDom.src = 'dice-' + dice + '.png';
+    diceDom.src = 'img/dice-' + dice + '.png';
     //3. add random no. until 1
     if (dice !== 1) {
         //add score
